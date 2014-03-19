d7fc8dc5ca8f9e6bad826ef65c55e95f091bef1f
David King <David.King@open.ac.uk>
2014-03-19 16:49:17 +0000

    data loading
    experiments in reading remote js files

--- data_visualisation/test_pages/c.js
+++ data_visualisation/test_pages/c.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-    /* $('#result').load('http://vbrant.ipd.uka.de/RefBank/rbk/name'); */
-    $.get('vbrant.ipd.uka.de/RefBank/rbk/name', 'jsonp'), function(data) {
-        $('#result').html(data);
-        $.print('Load was performed.');
-        $.print(data);
-    };
-    $('#result').html('<em>some stuff</em>');
+    $("#pressme").click(function() {
+        var d =  new Date();
+        $( "#runme" )
+        .replaceWith( "<script id=\"runme\" src=\"http://localhost/amati/data.js?"+d.getTime()+"\"></script>" )
+        .ready(function() {
+            $( "#result" ).replaceWith( "<p id=\"result\">"+d.getTime()+"</p>" )
+            .ready(function() {
+                $( "#result" )
+                .text( test_data );
+            });
+        });
+    });
+
 });
---
+++ data_visualisation/test_pages/data.js
@@ -0,0 +1,2 @@
+var test_data = "Mary had a little lamb";
+// alert("Hello from data");
